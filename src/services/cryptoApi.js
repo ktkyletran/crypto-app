1c61f7a5888ac638e454ccfb59ec7ea983e93fbd
Vanity <ktkyletran@gmail.com>
2022-04-04 19:37:42 -0500

    removed

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
   'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-  'X-RapidAPI-Key': '597af6d508msh4f437f40e535ef2p15d49ejsnefd36dd2c4a6'
+  'X-RapidAPI-Key': ''
 };
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com/'
